f20498d680550e6d16de5657a5cb4badb21077f9
Filipp <sap.phil.v@mail.ru>
2023-09-07 21:50:10 +0300

    первая рабочая версия чтения строк

--- src/main.cpp
+++ src/main.cpp
@@ -1,173 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include <stdarg.h>
+#include <string.h>
 
-int *matrix_create(size_t sizeRows, size_t sizeCols);
-
-void matrix_delete( int *matrix );
-
-int matrix_get_elem( const int *arr, size_t indRow, size_t indCol );
-
-int matrix_set_elem( int *arr, size_t indRow, size_t indCol, int new_value );
-
-int *matrix_get_ptr_elem( int *arr, size_t indRow, size_t indCol );
-
-void matrix_print( int *arr );
-
-size_t matrix_get_width(const int* arr );
-
-size_t matrix_get_height(const int* arr );
-
-size_t matrix_get_fact_len( size_t sizeRows, size_t sizeCols );
-
-size_t find_maximum_elem_width(const int *arr);
-
-size_t find_num_width(int num);
-
-inline size_t matrix_get_index__(const int *arr, size_t indRow, size_t indCol )
-{
-    return indRow*matrix_get_width(arr) + indCol + 2;
-}
+void print_text(char *text[], size_t nLines);
 
 int main()
 {
-    size_t rows = 0, cols = 0;
+    const size_t nLines = 5;
+    const size_t BUF_SIZE = 100;
 
-    scanf("%d %d", &rows, &cols);
+    char *text[nLines] = {};
 
-    int *arr2d = matrix_create(rows, cols);
-
-    for (size_t indRow = 0; indRow < rows; indRow++)
+    char buf[BUF_SIZE] = {};
+    for (size_t ind = 0; ind < nLines; ind++)
     {
-        for (size_t indCol = 0; indCol < cols; indCol++)
-        {
-            matrix_set_elem(arr2d, indRow, indCol, (indRow*cols + indCol)*(indRow*cols + indCol) );
-        }
+        if (fgets(buf, BUF_SIZE, stdin) == NULL) break;
+
+        size_t str_in_buf_len = strlen(buf);
+
+        text[ind] = (char *) calloc( str_in_buf_len + 1, sizeof(char) );
+
+        strncpy( text[ind], buf, (str_in_buf_len + 1) - 1 );
+        text[ind][str_in_buf_len] = '\0'; //< in case read line is bigger than buf
     }
 
-    matrix_print(arr2d);
-
-    matrix_delete(arr2d);
+    print_text(text, nLines);
 
     return 0;
 }
 
-int *matrix_create(size_t sizeRows, size_t sizeCols)
+void print_text(char *text[], size_t nLines)
 {
-    assert(sizeRows > 0);
-    assert(sizeCols > 0);
+    assert(text != NULL);
+    assert(nLines > 0);
 
-    int *p_arr = (int *) calloc( matrix_get_fact_len(sizeRows, sizeCols), sizeof(int) );
-
-    p_arr[0] = sizeRows;
-    p_arr[1] = sizeCols;
-
-    return p_arr;
-}
-
-void matrix_delete( int *matrix )
-{
-    assert( matrix != NULL );
-
-    free(matrix);
-}
-
-int matrix_get_elem( const int *arr, size_t indRow, size_t indCol )
-{
-    assert(arr != NULL);
-    assert(indRow < matrix_get_height(arr));
-    assert(indCol < matrix_get_width(arr));
-
-    return arr[ matrix_get_index__(arr, indRow, indCol) ];
-}
-
-int matrix_set_elem( int *arr, size_t indRow, size_t indCol, int new_value )
-{
-    assert(arr != NULL);
-    assert(indRow < matrix_get_height(arr));
-    assert(indCol < matrix_get_width(arr));
-
-    int old_value = arr[matrix_get_index__(arr, indRow, indCol)];
-    arr[matrix_get_index__(arr, indRow, indCol)] = new_value;
-    return old_value;
-}
-
-int *matrix_get_ptr_elem( int *arr, size_t indRow, size_t indCol )
-{
-    assert(arr != NULL);
-    assert(indRow < matrix_get_height(arr));
-    assert(indCol < matrix_get_width(arr));
-
-    return &arr[matrix_get_index__(arr, indRow, indCol)];
-}
-
-void matrix_print( int *arr )
-{
-    assert(arr != NULL);
-
-    size_t max_width = find_maximum_elem_width( arr );
-
-    for (size_t indRow = 0; indRow < matrix_get_height(arr); indRow++)
+    for (size_t ind = 0; ind < nLines; ind++)
     {
-        for (size_t indCol = 0; indCol < matrix_get_width(arr); indCol++)
+        if (text[ind] != NULL)
         {
-            printf("%*d ", max_width, matrix_get_elem(arr, indRow, indCol));
+            printf("At address %p lies string: <%s>\n", text[ind], text[ind]);
         }
-        printf("\n");
-    }
-}
-
-size_t matrix_get_width(const int* arr )
-{
-    assert(arr != NULL);
-
-    return (size_t) arr[1];
-}
-
-size_t matrix_get_height(const int* arr )
-{
-    assert(arr != NULL);
-
-    return (size_t) arr[0];
-}
-
-size_t matrix_get_fact_len( size_t sizeRows, size_t sizeCols )
-{
-    return sizeRows*sizeCols + 2;
-}
-
-size_t find_maximum_elem_width(const int *arr)
-{
-    assert(arr != NULL);
-
-    size_t ans = 0;
-    for (size_t indRow = 0; indRow < matrix_get_height(arr); indRow++)
-    {
-        for (size_t indCol = 0; indCol < matrix_get_width(arr); indCol++)
+        else
         {
-            size_t res = find_num_width( matrix_get_elem( arr, indRow, indCol ) );
-            if (res > ans) ans = res;
+            printf("At index %ud lies nullptr!\n", ind);
         }
+
     }
-    return ans;
-}
-
-size_t find_num_width(int num)
-{
-    size_t res = 0;
-
-    if (num < 0)
-    {
-        res++;
-        num = -num;
-    }
-
-    while( num > 0 )
-    {
-        res++;
-        num = num/10;
-    }
-
-    return res;
 }
