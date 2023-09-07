af7ebb2fc902004c8916d93f6bf7b010b379318f
Filipp <sap.phil.v@mail.ru>
2023-09-07 23:08:17 +0300

    переход на my_getline

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,23 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include "mystring.h"
 
 size_t get_number_of_lines(void);
+
+/*!
+    @brief Allocates memory and sets pointers (elements of text[])
+    to corresponding blocks of memory.
+    @param [in] text[] Array of pointers to char. All pointers will point to
+    corresponding blocks of allocated memory.
+    @param [in] nLines Number of elements in text[], and also number of lines
+    to be stored.
+    @param [in] line_size Size (in chars) of one block of memory to allocate.
+    Later this block will be changed by my_getline() if needed, but my_getline()
+    requires some memory to start with. Must be > 2.
+    @return text[] if no errors occurred, NULL otherwise.
+*/
+char ** initialize_text(char *text[], size_t nLines, size_t line_size);
 
 void print_text(char *text[], size_t nLines);
 
@@ -17,24 +32,27 @@ void inline clear_inp_buf(FILE *inp_stream)
 
 int main()
 {
-    size_t nLines = 5;
-    const size_t BUF_SIZE = 100;
+    const size_t START_LINE_SIZE = 10;
 
-    nLines = get_number_of_lines();
+    size_t nLines = get_number_of_lines();
 
     char **text = (char **) calloc( nLines, sizeof( char * ) );
 
-    char buf[BUF_SIZE] = {};
+    initialize_text(text, nLines, START_LINE_SIZE);
+
     for (size_t ind = 0; ind < nLines; ind++)
     {
-        if (fgets(buf, BUF_SIZE, stdin) == NULL) break;
+        size_t n_bytes = START_LINE_SIZE;
+        size_t read_bytes = my_getline(&text[ind], &n_bytes, stdin);
 
-        size_t str_in_buf_len = strlen(buf);
+        if (read_bytes == EOF)
+        {
+            printf("EOF! Stop.\n");
+            break;
+        }
 
-        text[ind] = (char *) calloc( str_in_buf_len + 1, sizeof(char) );
-
-        strncpy( text[ind], buf, (str_in_buf_len + 1) - 1 );
-        text[ind][str_in_buf_len] = '\0'; //< in case read line is bigger than buf
+        printf("!   For line at index %u %u bytes were read. "
+        "It takes %u bytes in memory.\n", ind, read_bytes, n_bytes);
     }
 
     print_text(text, nLines);
@@ -62,6 +80,22 @@ size_t get_number_of_lines(void)
     clear_inp_buf(stdin);
 
     return nLines;
+}
+
+char ** initialize_text(char *text[], size_t nLines, size_t line_size)
+{
+    assert(text != NULL);
+    assert(nLines > 0);
+    assert(line_size > 2);
+
+    for (size_t ind = 0; ind < nLines; ind++)
+    {
+        text[ind] = (char *) calloc( line_size, sizeof(char) );
+
+        if ( text[ind] == NULL ) return NULL; // вероятно не хватило памяти
+    }
+
+    return text;
 }
 
 void print_text(char *text[], size_t nLines)
